f6a2c6f26329d9460118749328b7882ed94fda43
superhorse622 <masonrose622@gmail.com>
2023-09-27 03:00:40 -0400

    update webdriver

--- Chating.py
+++ Chating.py
@@ -271,8 +271,8 @@ class Chating:
             print(url)
             chrome_options = webdriver.ChromeOptions()
             chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--no-sandbox")
-            chrome_options.add_argument("--disable-dev-shm-usage")
+            # chrome_options.add_argument("--no-sandbox")
+            # chrome_options.add_argument("--disable-dev-shm-usage")
 
             browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
