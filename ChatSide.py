32dd3b0c5968eb8e41e3eed9983043277661375e
superhorse622 <masonrose622@gmail.com>
2023-10-03 06:00:58 -0400

    fix webdriver

--- ChatSide.py
+++ ChatSide.py
@@ -318,8 +318,6 @@ class Chating:
 
             options = webdriver.ChromeOptions()
             options.add_argument('--headless')
-            options.add_argument("--remote-debugging-port=9222")
-            options.add_argument('--disable-dev-shm-usage')
             options.add_argument('--no-sandbox')
 
             # options.add_argument('--no-sandbox') 
